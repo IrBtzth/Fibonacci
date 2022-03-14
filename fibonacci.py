0d7268de9524ac3102f15cc86e63716cc4e462d7
betza <betzabeth.btzth@gmail.com>
2022-03-14 18:12:20 -0400

    Added descriptions in fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,20 @@
 import time
-
+#An iterator is a data structure for storing infinite data. 
 class FiboIter():
-
+    #Class constructor
     def __init__(self, max=None):
         self.max = max       
 
     def __iter__(self):
+    #The iter method is used to have elements or attributes that will be needed for the iterator to work.
         self.n1 = 0
         self.n2 = 1
         self.counter = 0
         return self
 
     def __next__(self):
-        
+    #The next method is the one needed to have the next function in Python.
+    #This method allows you to extract each of the iterator elements.   
         if self.counter == 0:
             self.counter += 1
             return self.n1
